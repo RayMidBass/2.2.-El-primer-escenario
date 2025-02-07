9d7bfad8bf7f4f916f44a96ff67bfc31472ef256
RayMidBass <99561580+RayMidBass@users.noreply.github.com>
2025-02-07 10:15:28 -0600

    Initial commit

---
+++ Assets/Scripts/OpenLink2.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink2 : MonoBehaviour
+{
+  public void OpenFacebook(){
+    Application.OpenURL("https://www.facebook.com");
+   }
+}
---
+++ Assets/Scripts/OpenLink3.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink3 : MonoBehaviour
+{
+     public void OpenInstagram(){
+    Application.OpenURL("https://www.instagram.com");
+   }
+}
